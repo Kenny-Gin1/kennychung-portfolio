9a0c03db0108b4f1d11810e23a32fc045eb59670
Kenny Chung <kenny.chung@appliedtechnology.se>
2020-07-17 13:57:36 +0200

    made the navbar responsive

--- components/SideBar.js
+++ components/SideBar.js
@@ -1,26 +1,49 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
 import Drawer from '@material-ui/core/Drawer'
 import ListItems from './ListItems'
 import SocialMedia from './SocialMediaIcons'
 import HeaderImage from './HeaderImage'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import CloseIcon from '@material-ui/icons/Close'
+import Hidden from '@material-ui/core/Hidden'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
 
-const drawerWidth = '18vw'
+const drawerWidth = 240
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
   },
   drawer: {
-    width: drawerWidth,
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    backgroundColor: '#b33939',
   },
   drawerPaper: {
     width: drawerWidth,
     backgroundColor: '#b33939',
-    marginLeft: theme.spacing(5),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+    closeMenuButton: {
+      marginRight: 'auto',
+      marginLeft: 0,
+    },
   },
   // necessary for content to be below app bar
-  //toolbar: theme.mixins.toolbar,
+  toolbar: theme.mixins.toolbar,
   content: {
     backgroundColor: theme.palette.background.default,
     padding: theme.spacing(3),
@@ -29,21 +52,73 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SideBar(props) {
   const classes = useStyles()
+  const theme = useTheme()
+  const [mobileOpen, setMobileOpen] = React.useState(false)
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen)
+  }
+
   return (
     <div className={classes.root}>
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-        anchor="left"
-        {...props}
-      >
-        <HeaderImage />
-        <ListItems />
-        <SocialMedia />
-      </Drawer>
+      <Hidden smUp implementation="css">
+        <AppBar position="fixed" className={classes.appBar}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="Open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              className={classes.menuButton}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap>
+              Home
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          className={classes.drawer}
+          variant="temporary"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+          anchor="left"
+          {...props}
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+        >
+          <IconButton
+            onClick={handleDrawerToggle}
+            className={classes.closeMenuButton}
+          >
+            <CloseIcon />
+          </IconButton>
+
+          <HeaderImage />
+          <ListItems />
+          <SocialMedia />
+        </Drawer>
+      </Hidden>
+
+      <Hidden xsDown implementation="css">
+        <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <div className={classes.toolbar} />
+          <HeaderImage />
+          <ListItems />
+          <SocialMedia />
+        </Drawer>
+      </Hidden>
     </div>
   )
 }
